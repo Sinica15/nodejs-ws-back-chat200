1dbcc386d23ecef5d6aa303007c3822236e59d1c
Sinica15 <ruja_vl@mail.ru>
2019-08-09 13:07:40 +0300

    add config

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const WebSocket = require('ws');
-// import express from 'express';
+const path = require('path');
+const fs = require('fs');
 
 const PORT = 9004;
 
 let app = express();
 
 app.use(express.static('public'));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// app.get('/', function (req, res) {
-//     res.send('Hello');
-// });
+app.get('/settings', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/settings.html'));
+    console.log("settings doing");
+    // res.sendFile('public/settings.html');
+});
+
+app.get('/setconfig', function (req, res) {
+    res.json(getConfig());
+});
+
+function getConfig() {
+    let configPath = 'public/chat_settings';
+    let readJsonConfig = path => JSON.parse(fs.readFileSync(path, 'utf8'));
+    if (fs.existsSync(configPath + '.json'))
+        return readJsonConfig(configPath + '.json');
+    else
+        return readJsonConfig(configPath + '_default.json');
+}
+
+app.post('/settings', function (req, res) {
+    console.log(req.body);
+    fs.writeFile('public/chat_settings.json', JSON.stringify(req.body), 'utf8', () => {});
+    res.json(req.body);
+});
 
 app.listen(PORT, function () {
     console.log('web started on ' + PORT);
@@ -24,8 +48,9 @@ wss.on('connection', ws => {
         console.log(`Received message => ${message}`);
         if(message && JSON.parse(message).message){
             setTimeout(() =>{
-                ws.send(formingMsgJSON(JSON.parse(message).message.toUpperCase(), 'upperBot'));
-            }, 5000);
+                let config = getConfig();
+                ws.send(formingMsgJSON('Ответ на "' + JSON.parse(message).message.toUpperCase() + '"', config['bot_name']));
+            }, 2500);
         }
     });
     ws.on('close', () =>{
